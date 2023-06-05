2793f03af3844aaa17c246297091aa41328c3dd5
kimmingyun <mingyun8646@nate.com>
2023-06-05 17:29:04 +0900

    feat : POST API - content : post, parsing, @Data

---
+++ src/main/java/com/example/selfstudy/controller/PostApiController.java
@@ -0,0 +1,45 @@
+package com.example.selfstudy.controller;
+
+import com.example.selfstudy.dto.PostRequestDto;
+import com.example.selfstudy.dto.PostRequestDto2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post01")
+    public void post(@RequestBody Map<String, Object> requestData){
+        requestData.forEach((key, value) ->{
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
+    /* 위 코드는 람다식 적용한거. 람다식 적용안한 코드는
+        public void post(@RequestBody Map<String, Object> requestData) {
+        for (Map.Entry<String, Object> entry : requestData.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        }
+    }
+     */
+    @PostMapping("/post02")
+    public void post(@RequestBody PostRequestDto requestData){
+        System.out.println(requestData);
+    }
+    @PostMapping("/post03")
+    public void post(@RequestBody PostRequestDto2 requestData){
+        log.info("USER : {}", requestData);
+
+    }
+}
---
+++ src/main/java/com/example/selfstudy/dto/PostRequestDto2.java
@@ -0,0 +1,20 @@
+package com.example.selfstudy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class PostRequestDto2 {
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @JsonProperty("OTP")
+    private String OTP;
+
+
+}
